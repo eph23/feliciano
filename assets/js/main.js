d43b69ab2ef1b5581bfae331ffd2be09e0d32469
Ephraim <ephraims.818@gmail.com>
2019-11-07 22:49:54 +0600

    First Commit

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+$('.hero-carousel').owlCarousel({
+  loop: true,
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  margin: 0,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
+})
