036d8cf69b57ba446d375f1fa8b173a7ba2d2c5b
Ephraim <ephraims.818@gmail.com>
2019-11-21 10:17:50 +0600

    Hero Carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@ $('.hero-carousel').owlCarousel({
   autoplayTimeout: 3000,
   margin: 0,
   nav: false,
+  touchDrag: true,
   responsive: {
     0: {
       items: 1
@@ -14,6 +15,25 @@ $('.hero-carousel').owlCarousel({
     },
     1000: {
       items: 1
+    }
+  }
+})
+$('.carousel-testimonial').owlCarousel({
+  loop: true,
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  margin: 0,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 3
     }
   }
 })
